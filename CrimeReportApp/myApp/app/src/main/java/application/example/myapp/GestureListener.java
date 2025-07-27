97cc37aa9313a775b364c0eb09171fe4b71e8311
BALAGANESH S B <sbbalaganesh2004@gmail.com>
2025-07-27 21:08:57 +0530

    third commit

--- CrimeReportApp/myApp/app/src/main/java/application/example/myapp/GestureListener.java
+++
@@ -1,4 +0,0 @@
-package application.example.myapp;
-
-class GestureListener {
-}
--- CrimeReportApp/myApp/app/src/main/java/application/example/myapp/sos1.java
+++
@@ -1,4 +0,0 @@
-SMTP :Server smtp-relay.brevo.com
-Port :587
-Login :84ff3a001@smtp-brevo.com
-Pass: ScRXbOds2Irhp1k0
